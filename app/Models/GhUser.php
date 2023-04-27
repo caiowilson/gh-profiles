b3c4619332eed0ec113dfdd14201c099a7da93f3
Caio Wilson <caiowilson@gmail.com>
2023-04-27 01:37:56 -0300

    ADD github User model

--- app/Models/GhUser.php
+++ app/Models/GhUser.php
@@ -2,11 +2,23 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class GhUser extends Model
 {
     use HasFactory;
     
+    protected $guarded = ['id'];
+   
+    /**
+     * Get all of the repositories for the GhUser
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function repositories(): HasMany
+    {
+        return $this->hasMany(GhRepository::class, 'id', 'gh_user_id');
+    }
 }
